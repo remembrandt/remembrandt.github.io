43396329d6e4db853448770bca1f70062a03ae4a
remembrandt <fopeyoxila@9me.site>
2017-02-11 05:34:42 +0300

    Update visualizer.js

--- viz/js/visualizer.js
+++ viz/js/visualizer.js
@@ -73,7 +73,7 @@ AudioVisualizer.prototype = {
       var rgba = [data[i], data[i+1], data[i+2], data[i+3]];
       var avg = (rgba[0] + rgba[1] + rgba[2]) / 3;
       if (avg < averageLighting) {
-        ctx.clearRect(x,y,1,1);
+        ctx.strokeRect(x,y,1,1);
          ctx.fillStyle = "#8BC34A";
       }
 
